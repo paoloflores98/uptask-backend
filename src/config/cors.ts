d76893d8c6eef4e46ed5c069d2548aa1bf44d262
Paolo Flores <maximo_150@hotmail.com>
2024-09-28 16:18:29 -0500

    fix cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -4,10 +4,6 @@ export const corsConfig: CorsOptions = {
   origin: function(origin, callback) {
     const whiteList = [process.env.FRONTEND_URL];
 
-    // Para depuración: Log de origen y whiteList
-    console.log("Origin:", origin);
-    console.log("WhiteList:", whiteList);
-
     // process.arg[2]: Verifica si el tercer argumento pasado al ejecutar un script de Node.js es la cadena "--api" (Puede ser otra)
     if(process.argv[2] === '--api') {
       whiteList.push(undefined);
@@ -17,7 +13,6 @@ export const corsConfig: CorsOptions = {
     if(whiteList.includes(origin)) {
       callback(null, true);
     }else {
-      console.log("Origen no permitido:", origin); // Para ayudar a depurar
       callback(new Error('Error de CORS'));
     }
   }
