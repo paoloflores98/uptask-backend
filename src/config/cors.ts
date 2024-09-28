731c79008b71b69f2c51fff9ffde70c21c18fb9f
Paolo Flores <maximo_150@hotmail.com>
2024-09-28 11:53:34 -0500

    cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,6 +3,7 @@ import { CorsOptions } from "cors";
 export const corsConfig: CorsOptions = {
   origin: function(origin, callback) {
     const whiteList = [process.env.FRONTEND_URL];
+    console.log(whiteList);
     // process.arg[2]: Verifica si el tercer argumento pasado al ejecutar un script de Node.js es la cadena "--api" (Puede ser otra)
     if(process.argv[2] === '--api') {
       whiteList.push(undefined);
